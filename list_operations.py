0cc84d2b9236a00569c3c563b4efc2d2589bff88
Vicki Erjavec <verjavec@gmail.com>
2021-01-08 07:27:24 +0000

    Complete functions

--- list_operations.py
+++ list_operations.py
@@ -10,8 +10,7 @@ def head(input_list):
       'Jan'
     """
 
-    return None
-
+    return input_list [0]
 
 def tail(input_list):
     """Return a new list of all items, excluding the first item.
@@ -23,7 +22,7 @@ def tail(input_list):
 
     """
 
-    return []
+    return input_list[1:]
 
 
 def last(input_list):
@@ -36,7 +35,7 @@ def last(input_list):
 
     """
 
-    return []
+    return input_list[-1]
 
 
 def top(input_list):
@@ -49,7 +48,7 @@ def top(input_list):
 
     """
 
-    return []
+    return input_list[:-1]
 
 
 def first_three(input_list):
@@ -62,7 +61,7 @@ def first_three(input_list):
 
     """
 
-    return []
+    return input_list[:3]
 
 
 def last_five(input_list):
@@ -75,7 +74,7 @@ def last_five(input_list):
 
     """
 
-    return []
+    return input_list[5:]
 
 
 def middle(input_list):
@@ -88,7 +87,7 @@ def middle(input_list):
 
     """
 
-    return []
+    return input_list[2:-2]
 
 
 def inner_four(input_list):
@@ -101,7 +100,7 @@ def inner_four(input_list):
 
     """
 
-    return []
+    return input_list[2:6]
 
 
 def inner_four_end(input_list):
@@ -117,7 +116,7 @@ def inner_four_end(input_list):
 
     """
 
-    return []
+    return input_list[4:8]
 
 
 def replace_head(input_list):
@@ -132,6 +131,7 @@ def replace_head(input_list):
 
     """
 
+    input_list[0]=42
     pass
 
 
@@ -146,7 +146,8 @@ def replace_third_and_last(input_list):
     True
 
     """
-
+    input_list[2] = 37
+    input_list[-1] = 37
     pass
 
 
@@ -165,7 +166,7 @@ def replace_middle(input_list):
     True
 
     """
-
+    input_list[2:-2] = [42, 37]
     pass
 
 
@@ -180,6 +181,8 @@ def delete_third_and_seventh(input_list):
     True
 
     """
+    del input_list[2]
+    del input_list[-2]
 
     pass
 
@@ -198,6 +201,7 @@ def delete_middle(input_list):
 
     """
 
+    del input_list[2:-2]
     pass
 
 
